6b06229ae94a1e20f78e588e6bdc21ba137fca09
Hemanth-Sappa <134211683+Hemanth-Sappa@users.noreply.github.com>
2023-05-22 18:35:44 +0530

    Update GameFrame.java

--- Pong Game/src/GameFrame.java
+++ Pong Game/src/GameFrame.java
@@ -11,9 +11,9 @@ public class GameFrame extends JFrame {
         add(panel);
         pack();
         setResizable(false);
-        setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
